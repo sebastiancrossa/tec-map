358eb39e3a1e2bc121170041a5efc42eb2ab703f
Sebastian Crossa <crossasebastian@gmail.com>
2020-05-19 16:26:59 -0600

    Implementacion de mas lugares. Refactor para desplegar los poligonos de forma automatica

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,12 +1,6 @@
 // Libraries
 import React, { useState } from "react";
-import {
-  Map as LeafletMap,
-  TileLayer,
-  Marker,
-  Popup,
-  Polygon,
-} from "react-leaflet";
+import { Map as LeafletMap, TileLayer, Tooltip, Polygon } from "react-leaflet";
 
 const Leaflet = window.L;
 
@@ -16,15 +10,41 @@ const Map = () => {
     lat: 20.735007,
     lng: -103.456181,
     zoom: 17,
-    lugares: {
-      centroCongresos: [
-        [20.732895, -103.456267],
-        [20.73248, -103.455965],
-        [20.732869, -103.45541],
-        [20.7333, -103.455776],
-      ],
-    },
+    lugares: [
+      {
+        name: "Centro de Congresos",
+        color: "blue",
+        coords: [
+          [20.732895, -103.456267],
+          [20.73248, -103.455965],
+          [20.732869, -103.45541],
+          [20.7333, -103.455776],
+        ],
+      },
+      {
+        name: "Gimnasio",
+        color: "red",
+        coords: [
+          [20.736284, -103.455898],
+          [20.735463, -103.455966],
+          [20.735404, -103.455617],
+          [20.736248, -103.455524],
+        ],
+      },
+      {
+        name: "Biblioteca",
+        color: "green",
+        coords: [
+          [20.735269, -103.455015],
+          [20.734837, -103.455036],
+          [20.734782, -103.454457],
+          [20.735184, -103.454446],
+        ],
+      },
+    ],
   });
+
+  console.log(mapPrefs.lugares.map((lugar) => console.log(lugar)));
 
   // Inicializacion de toda la info que nos sirve para denotar el mapa
   const position = [mapPrefs.lat, mapPrefs.lng];
@@ -47,7 +67,13 @@ const Map = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      <Polygon positions={mapPrefs.lugares.centroCongresos} color="blue" />
+      {mapPrefs.lugares.map((lugar) => (
+        <Polygon positions={lugar.coords} color={lugar.color}>
+          <Tooltip direction="bottom" sticky>
+            {lugar.name}
+          </Tooltip>
+        </Polygon>
+      ))}
     </LeafletMap>
   );
 };
