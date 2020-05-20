43fd50f58abd716c97fe5a4f72cfbda635ee9851
Sebastian Crossa <crossasebastian@gmail.com>
2020-05-19 19:56:28 -0600

    Implementacion de listado de los establecimientos con sus colores respectivos

--- src/styles/mapView.style.js
+++ src/styles/mapView.style.js
@@ -17,6 +17,29 @@ export const SpacesListContainer = styled.div`
   -webkit-box-shadow: 0px 0px 80px -15px rgba(136, 186, 255, 0.34);
   -moz-box-shadow: 0px 0px 80px -15px rgba(136, 186, 255, 0.34);
   box-shadow: 0px 0px 80px -15px rgba(136, 186, 255, 0.34);
+
+  h1 {
+    font-size: 1.2rem;
+  }
+`;
+
+export const Establecimiento = styled.div`
+  display: flex;
+  align-items: center;
+
+  padding: 1rem;
+  border-radius: 5px;
+
+  .circle {
+    width: 1rem;
+    height: 1rem;
+
+    margin-right: 0.5rem;
+
+    border-radius: 50rem;
+
+    background-color: ${({ color }) => color && color};
+  }
 `;
 
 export const MapContainer = styled.div`
