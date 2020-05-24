815c619b58205eb6703bfd0f59ae216db51d2207
Sebastian Crossa <crossasebastian@gmail.com>
2020-05-24 01:08:09 -0600

    Implementacion de visualizacion de lugares en modo lista

--- src/pages/ListView.jsx
+++ src/pages/ListView.jsx
@@ -1,18 +1,132 @@
 // Libraries
-import React from "react";
+import React, { useState } from "react";
+import { lugares } from "../utils/db";
 
 // Styles
+import { Stack, Collapse, Checkbox, Icon, Button, Tag } from "@chakra-ui/core";
 import { OuterContainer as Container } from "../style";
-import { Header } from "../styles/listView.style";
+import {
+  Header,
+  SpacesContainer,
+  Establecimiento,
+} from "../styles/listView.style";
 
 // Component Imports
 import Layout from "../components/layout";
 
 const ListView = () => {
+  // ! REFACTOR THE FILTERING CODE SO THE LOGIC IS CONTAINED IN ONE FILE (OR MOST OF IT)
+
+  const [show, setShow] = useState(false);
+  const [onCheck, setOnCheck] = useState(false);
+  const [filterValue, setFilterValue] = useState([]);
+
+  const handleToggle = () => setShow(!show);
+
+  const handleCheckboxToggle = (e, i) => {};
+
   return (
     <Layout>
       <Container>
+        <div style={{ marginBottom: "1rem" }}>
+          <Button onClick={handleToggle} style={{ backgroundColor: "#EDF2F7" }}>
+            <Icon name="settings" />
+          </Button>
+          <Collapse
+            mt={4}
+            isOpen={show}
+            style={{
+              backgroundColor: "#EDF2F7",
+              padding: "1rem",
+              borderRadius: "5px",
+            }}
+          >
+            Filtrar por:
+            <Stack isInline spacing={4}>
+              <Checkbox
+                value="bathroom"
+                isChecked={filterValue.includes("bathroom")}
+                onChange={(e) => handleCheckboxToggle(e, "bathroom")}
+              >
+                Baños
+              </Checkbox>
+              <Checkbox
+                value="computers"
+                isChecked={filterValue.includes("computers")}
+                onChange={(e) => handleCheckboxToggle(e, "computers")}
+              >
+                Computadoras
+              </Checkbox>
+              <Checkbox
+                value="printers"
+                isChecked={filterValue.includes("printers")}
+                onChange={(e) => handleCheckboxToggle(e, "printers")}
+              >
+                Impresión
+              </Checkbox>
+              <Checkbox
+                value="open"
+                isChecked={filterValue.includes("printers")}
+                onChange={(e) => handleCheckboxToggle(e, "printers")}
+              >
+                Abierto
+              </Checkbox>
+            </Stack>
+          </Collapse>
+        </div>
+
         <Header>Establecimientos</Header>
+
+        <SpacesContainer>
+          {lugares.map((lugar) => (
+            <Establecimiento>
+              <h1 style={{ margin: "0" }}>{lugar.name}</h1>
+              <p style={{ marginBottom: "0.3rem" }}>{lugar.desc}</p>
+
+              {lugar.servicios.length > 0 && (
+                <Stack spacing={2} isInline>
+                  {lugar.servicios.map((servicio) => {
+                    if (servicio === "bathroom") {
+                      return (
+                        <Tag
+                          size="sm"
+                          style={{ backgroundColor: "#90CDF4" }}
+                          color="white"
+                        >
+                          Baños
+                        </Tag>
+                      );
+                    }
+
+                    if (servicio === "computers") {
+                      return (
+                        <Tag
+                          size="sm"
+                          style={{ backgroundColor: "#4FD1C5" }}
+                          color="white"
+                        >
+                          Computadoras
+                        </Tag>
+                      );
+                    }
+
+                    if (servicio === "printers") {
+                      return (
+                        <Tag
+                          size="sm"
+                          style={{ backgroundColor: "#FC8181" }}
+                          color="white"
+                        >
+                          Impresión
+                        </Tag>
+                      );
+                    }
+                  })}
+                </Stack>
+              )}
+            </Establecimiento>
+          ))}
+        </SpacesContainer>
       </Container>
     </Layout>
   );
--- src/styles/listView.style.js
+++ src/styles/listView.style.js
@@ -6,3 +6,26 @@ export const Header = styled.h1`
 
   margin-bottom: 0.5rem;
 `;
+
+export const Establecimiento = styled.div`
+  background-color: white;
+  border-radius: 5px;
+
+  padding: 1rem 2rem;
+
+  border-radius: 5px;
+
+  -webkit-box-shadow: 0px 0px 80px -15px rgba(136, 186, 255, 0.34);
+  -moz-box-shadow: 0px 0px 80px -15px rgba(136, 186, 255, 0.34);
+  box-shadow: 0px 0px 80px -15px rgba(136, 186, 255, 0.34);
+
+  h1 {
+    font-size: 1.2rem;
+  }
+`;
+
+export const SpacesContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-gap: 1.2rem;
+`;
